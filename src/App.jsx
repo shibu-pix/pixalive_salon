bbf8af4dc8cd1353b1698dfccc0f5f8e136fa471
gayathri23-y <gayudecember23@gmail.com>
2025-06-13 09:41:58 +0530

    sample project for pixalive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Product1Page from './components/product1/Product1Page';
 
 function App() {
   return (
     <div>
-      
+      <Product1Page />
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
